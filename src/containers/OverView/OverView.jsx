a29a5850c65038d35faa03b6da8612215e440584
Nishant Kumar <nishants878@gmail.com>
2021-05-31 20:04:20 +0530

    added order tab

--- src/containers/OverView/OverView.jsx
+++ src/containers/OverView/OverView.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import classes from './OverView.module.css';
 import OverviewSmallCards from '../../components/OverviewSmalCards/OverviewSmallCards';
 import PerformanceChart from '../../components/PerformaceChart/PerformanceChart';
-import StatisticsChart from '../../components/StatisticsChart/StatisticsChart'
+import StatisticsChart from '../../components/StatisticsChart/StatisticsChart';
+import listIcon from '../../Assests/list.svg';
+import gridIcon from '../../Assests/grid.svg';
+import OrderListCard from '../../components/OrderListCard/OrderListCard'
 export default function OverView() {
     return (
         <div className={classes.MainContainer}>
@@ -13,7 +16,51 @@ export default function OverView() {
          <PerformanceChart/><StatisticsChart/>
       </div>
       <div className={classes.BottomFlexWrapper}>
-      
+      <div className={classes.OrderHeader}>
+         <div className={classes.HeaderLeft}>Order List</div> 
+         <div className={classes.HeaderRight}>
+             <div className={classes.ListContainer}>
+              <div className={classes.ListSvgContainer}>
+                  <img src={listIcon} alt="List"/>
+              </div>
+              <div className={classes.HeaderList}>
+                  List
+              </div>
+             </div>
+             <div className={classes.GridContainer}>
+             <div className={classes.GridSvgContainer}>
+                  <img src={gridIcon} alt="List"/>
+              </div>
+              <div className={classes.HeaderGrid}>
+                 Grid
+              </div>
+             </div>
+             </div> 
+      </div>
+      <div className={classes.OrderSecondHeader}>
+          <div className={classes.OrderId}>
+              Order Id
+          </div>
+          <div className={classes.CustomerName}>
+              Customer
+          </div>
+          <div className={classes.OrderDate}>
+          Order Date
+          </div>
+          <div className={classes.OrderAmount}>
+          Order Amount
+          </div>
+          <div className={classes.Status}>
+          Status
+          </div>
+      </div>
+      <div className={classes.OrderCardContainer}>
+ <OrderListCard/>
+ <OrderListCard/>
+ <OrderListCard/>
+ <OrderListCard/>
+ <OrderListCard/>
+      </div>
       </div>
         </div>
     )
